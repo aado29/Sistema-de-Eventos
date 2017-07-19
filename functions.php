777e06450476f593ed82973fcfe4ad69dcf16530
Alberto Diaz <aado29@gmail.com>
2017-07-19 01:50:33 -0400

    added tree build type

--- functions.php
+++ functions.php
@@ -48,9 +48,56 @@
 		$sistem = new Sistem('events');
 		return $sistem->getByDate($date);
 	}
+
 	function getReportByRangeDate($date = null, $date_ = null) {
 		if (is_null($date) || is_null($date_))
 			return array();
 		$sistem = new Sistem('events');
 		return $sistem->getBetweenDates($date, $date_);
+	}
+
+	function getTreeType($fieldName, $label) {
+		$sistem = new Sistem('events_type');
+		$sistem->get(array('id', '>', 0));
+		$data = $sistem->data();
+		$output = '<div class="form-group">';
+			$output .= '<label for="' . $fieldName . '">' . $label . '</label>';
+			$output .= '<select class="form-control" name="' . $fieldName . '" id="' . $fieldName . '">';
+				$output .= treeToHTML(buildTree($data));
+			$output .= '</select>';
+		$output .= '</div>';
+		echo $output;
+	}
+
+	function treeToHTML(array $elements, $pass = 0) {
+		$output = '';
+		foreach ($elements as $element) {
+			$disabled = (!array_key_exists('children', $element)) ? '' : 'disabled';
+			$output .= '<option value="' . $element['id'] . '" ' . $disabled .'>';
+				$output .= str_repeat('- ', $pass) . $element['name'];
+			$output .= '</option>';
+			if (array_key_exists('children', $element)) {
+				$output .= treeToHTML($element['children'], $pass + 1);
+			}
+		}
+		return $output;
+	}
+
+	function buildTree(array $elements, $parentId = 0) {
+		$branch = array();
+		foreach ($elements as $element) {
+			$element = array(
+				'id' => $element->id,
+				'name' => $element->name,
+				'parent_id' => $element->parent_id
+			);
+			if ($element['parent_id'] == $parentId) {
+				$children = buildTree($elements, $element['id']);
+				if ($children) {
+					$element['children'] = $children;
+				}
+				$branch[] = $element;
+			}
+		}
+		return $branch;
 	}
